b8ecfa84098d2fc04d661ece793924f9c623ae92
RBhagat <bhagatrushikesh.5@gmail.com>
2022-12-05 12:33:41 -0600

    sh deleted

--- dockerkill.sh
+++
@@ -1,7 +0,0 @@
-for id in $(docker ps -q)
-  do                            
-    if [[ $(docker port "${id}") == 5000 ]]; then
-    echo "stopping container ${id}"
-    docker stop "${id}"
-  fi
- done
